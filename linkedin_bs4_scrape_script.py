68c3e96051a82a89b1c1f6e9359dbfe3cc6246ed
doniprastyo25 <donismsbanking@gmail.com>
2023-06-03 10:33:12 +0700

    init

---
+++ linkedin_bs4_scrape_script.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+from lxml import etree
+import requests
+import json
+
+class ScrapeBs4:
+    def __init__(self) -> None:
+        self._xpath_company = '//*[@id="main-content"]/section[1]/div/section[2]/div/div[1]/div/h4/div[1]/span[1]/a'
+        self._xpath_locations = '//*[@id="main-content"]/section[1]/div/section[2]/div/div[1]/div/h4/div[1]/span[2]'
+        self._xpath_seniority = '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[1]/span'
+        self._xpath_type = '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[2]/span'
+        self._xpath_job_function = '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[3]/span'
+        self._xpath_industries = '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[4]/span'
+        self.base_url = "https://www.linkedin.com/jobs/search?keywords=Node.js&location=Indonesia&geoId=102478259&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
+        self.exec_script()
+    
+    def _instance_url(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
+    
+    def _extract_title(self, soup, xpath_tag, tag_href):
+        title: list = []
+        tree = etree.HTML(str(soup))
+        select = tree.xpath(xpath_tag)
+        link_elements = soup.select_one(tag_href)
+        link = link_elements['href']
+        for i in select:
+            title.append(i.text)
+        cleaned_title = title[0].strip()
+        job: dict = {
+            "job": cleaned_title,
+            "company": self._get_link_job(link, self._xpath_company),
+            "locations": self._get_link_job(link, self._xpath_locations),
+            "seniority": self._get_link_job(link, self._xpath_seniority),
+            "job_role": self._get_link_job(link, self._xpath_job_function),
+            "contract": self._get_link_job(link, self._xpath_type),
+            "industries": self._get_link_job(link, self._xpath_industries),
+            "link": link,
+        }
+        return job
+
+    def _get_link_job(self, link, xpath):
+        soup = self._instance_url(link)
+        raw_value: list = []
+        tree = etree.HTML(str(soup))
+        select = tree.xpath(xpath)
+        for i in select:
+            raw_value.append(i.text)
+        cleaned_value = raw_value[0].strip()
+        return cleaned_value
+
+    
+    def exec_script(self):
+        soup = self._instance_url(self.base_url)
+        for i in range(1, 6):
+            title = self._extract_title(soup, f'//*[@id="main-content"]/section[2]/ul/li[{i}]/div/a/span', f'#main-content > section.two-pane-serp-page__results-list > ul > li:nth-child({i}) > div > a')
+            print(json.dumps(title, indent=3))
+
+
+if __name__ == "__main__":
+    ScrapeBs4()
---
+++ linkedin_selenium_scrape_script.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+from lxml import etree
+import time
+driver = webdriver.Chrome()
+
+class Scrape:
+    time_sleep = time.sleep(5)
+    actions = ActionChains(driver)
+    def __init__(self):
+        self.base_url = "https://www.linkedin.com"
+        self._instance_url()
+        time.sleep(10)
+        self.actions
+        self.exec_script()
+
+    def _instance_url(self):
+        driver.get(self.base_url)
+        self.time_sleep
+    
+    def _get_elements(self, xpath_element):
+        get_elements = driver.find_element('xpath', xpath_element)
+        return get_elements
+
+    def _get_elements_and_click(self, xpath_element):
+        get_elements = self._get_elements(xpath_element)
+        self.actions.click(get_elements).perform()
+    
+    def _get_elements_and_fill(self, xpath_elements, send_keys):
+        get_elements = self._get_elements(xpath_elements)
+        get_elements.send_keys(send_keys)
+    
+    def _get_all_tag(self):
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        return soup.prettify()
+    
+    def _extract_title(self, xpath_link):
+        list_title = []
+        soup_process = self._get_all_tag()
+        tree = etree.HTML(str(soup_process))
+        select = tree.xpath(xpath_link)
+        for i in select:
+            list_title.append(i.text)
+        cleaned_element = list_title[0].strip()
+        return cleaned_element
+
+    
+    def exec_script(self):
+        self._get_elements_and_click('/html/body/nav/ul/li[4]/a')
+        time.sleep(8)
+        self._get_elements_and_fill('//*[@id="job-search-bar-keywords"]','Django')
+        self._get_elements_and_click('//*[@id="jobs-search-panel"]/form/section[2]/button')
+        self._get_elements_and_fill('//*[@id="job-search-bar-location"]', 'indonesia')
+        self._get_elements_and_click('//*[@id="jobs-search-panel"]/form/button')
+        for i in range(1, 5):
+            title = self._extract_title(f'//*[@id="main-content"]/section[2]/ul/li[{i}]/div/a/span')
+            print(title)
+        time.sleep(20)
+
+if __name__ == "__main__":
+    Scrape()
